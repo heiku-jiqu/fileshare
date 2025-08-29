a6b544a1c6bdf4942b08d810f19040df2721c0b6
Heiser <heiser9295@hotmail.com>
2025-08-29 15:53:48 +0800

    mock up routes

--- main.go
+++ main.go
@@ -11,10 +11,9 @@ import (
 )
 
 func main() {
-	log.Print("Hello World!")
-
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServerFS(t.Website))
+	mux.HandleFunc("/login", unimplemented)
 	mux.Handle("/api/v1/", http.StripPrefix("/api/v1", NewApiRouter()))
 
 	s := &http.Server{
@@ -24,12 +23,16 @@ func main() {
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
+	slog.Info("Listening and serving...")
 	log.Fatal(s.ListenAndServe())
 }
 
 func NewApiRouter() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/healthcheck", healthcheck)
+	mux.HandleFunc("GET /{user}/files/", unimplemented)
+	mux.HandleFunc("POST /{user}/file/", unimplemented)    // initiate new upload
+	mux.HandleFunc("PUT /{user}/file/{id}", unimplemented) // complete upload?
 	return logger(mux)
 }
 func healthcheck(w http.ResponseWriter, r *http.Request) {
@@ -48,4 +51,8 @@ func logger(next http.Handler) http.Handler {
 		slog.Info("Request served.", "protocol", proto, "method", method, "url", url, "ip", ip, "matched", r.Pattern)
 	})
 
+}
+
+func unimplemented(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "unimplemented")
 }
