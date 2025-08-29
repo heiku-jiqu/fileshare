92877ce56937d25d3474e63c99c494497bf9f4d1
Heiser <heiser9295@hotmail.com>
2025-08-29 15:42:04 +0800

    refactor out api router

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 	"time"
 
@@ -13,10 +14,8 @@ func main() {
 	log.Print("Hello World!")
 
 	mux := http.NewServeMux()
-	mux.Handle("GET /", http.FileServerFS(t.Website))
-	mux.HandleFunc("GET /api/v1/healthcheck", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "ok")
-	})
+	mux.Handle("/", http.FileServerFS(t.Website))
+	mux.Handle("/api/v1/", http.StripPrefix("/api/v1", NewApiRouter()))
 
 	s := &http.Server{
 		Addr:           ":8080",
@@ -26,4 +25,27 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	log.Fatal(s.ListenAndServe())
+}
+
+func NewApiRouter() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthcheck", healthcheck)
+	return logger(mux)
+}
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "ok")
+}
+
+func logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var (
+			ip     = r.RemoteAddr
+			method = r.Method
+			url    = r.URL.String()
+			proto  = r.Proto
+		)
+		next.ServeHTTP(w, r)
+		slog.Info("Request served.", "protocol", proto, "method", method, "url", url, "ip", ip, "matched", r.Pattern)
+	})
+
 }
